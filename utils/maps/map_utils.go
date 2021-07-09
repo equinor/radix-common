a4ea97a5d24c0c54eab05fd850d6b6ae0233a8ae
Sergey Smolnikov <ssmol@equinor.com>
2021-07-09 14:59:54 +0200

    Added map utils

--- utils/maps/map_utils.go
+++ utils/maps/map_utils.go
@@ -1,24 +1,37 @@
 package maps
 
-// GetKeysFromByteMap Returns keys
+//GetKeysFromByteMap Returns keys
 func GetKeysFromByteMap(mapData map[string][]byte) []string {
-	keys := []string{}
+	var keys []string
 	for k := range mapData {
+		k := k
 		keys = append(keys, k)
 	}
 
 	return keys
 }
 
-// Merge two maps, preferring the right over the left
+//GetKeysFromStringMap Returns keys
+func GetKeysFromStringMap(mapData map[string]string) []string {
+	var keys []string
+	for k := range mapData {
+		k := k
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+//MergeStringMaps Merge two maps, preferring the right over the left
 func MergeStringMaps(left, right map[string]string) map[string]string {
 	result := make(map[string]string)
 
 	for key, rVal := range right {
+		rVal := rVal
 		result[key] = rVal
 	}
 
 	for key, lVal := range left {
+		lVal := lVal
 		if _, present := right[key]; !present {
 			result[key] = lVal
 		}
