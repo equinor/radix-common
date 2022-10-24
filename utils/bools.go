1e197db53ce2c1749c27e46a31bb50f65deaebc2
Sergey Smolnikov <ssmol@equinor.com>
2022-10-24 14:24:05 +0200

    Add utils (#10)
    * Added generic map utils
    
    * Added generic pointer utils

--- utils/bools.go
+++ utils/bools.go
@@ -1,6 +1,6 @@
 package utils
 
-// BoolPtr returns a pointer to the passed bool.
+// BoolPtr returns a pointer to the passed bool. Deprecated - use pointers.Ptr
 func BoolPtr(value bool) *bool {
 	return &value
 }
--- utils/numbers/number_utils.go
+++ utils/numbers/number_utils.go
@@ -1,16 +1,16 @@
 package numbers
 
-// Int32Ptr converts an int32 to *int32
+// Int32Ptr converts an int32 to *int32. Deprecated - use pointers.Ptr
 func Int32Ptr(n int32) *int32 {
 	return &n
 }
 
-// Int64Ptr converts an int64 to *int64
+// Int64Ptr converts an int64 to *int64. Deprecated - use pointers.Ptr
 func Int64Ptr(n int64) *int64 {
 	return &n
 }
 
-// IntPtr Helper function to get the pointer of an int
+// IntPtr Helper function to get the pointer of an int. Deprecated - use pointers.Ptr
 func IntPtr(i int) *int {
 	return &i
 }
