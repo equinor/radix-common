1eb8f9f6452982731baa77ef2c9130c0420d003b
Nils Gustav Stråbø <nst@equinor.com>
2023-01-17 10:29:40 +0100

    add generic All and Any functions for slice

--- utils/slice/utils.go
+++ utils/slice/utils.go
@@ -14,6 +14,7 @@ func PointersOf(v interface{}) interface{} {
 	return out.Interface()
 }
 
+// Projects each element of a slice into a new form.
 func Map[T, V any](source []T, mapper func(T) V) []V {
 	result := make([]V, len(source))
 
@@ -22,4 +23,19 @@ func Map[T, V any](source []T, mapper func(T) V) []V {
 	}
 
 	return result
+}
+
+// Determines whether any element of a slice satisfies a condition.
+func Any[T any](source []T, predicate func(T) bool) bool {
+	for _, v := range source {
+		if predicate(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// Determines whether all elements of a slice satisfy a condition.
+func All[T any](source []T, predicate func(T) bool) bool {
+	return !Any(source, func(v T) bool { return !predicate(v) })
 }
