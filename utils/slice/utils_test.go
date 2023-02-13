0d77f02af8c73fbea89a86af79d43f27f6978993
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-02-13 13:41:37 +0100

    add tests

--- utils/slice/utils_test.go
+++ utils/slice/utils_test.go
@@ -23,33 +23,6 @@ func Test_Map(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func Test_Any(t *testing.T) {
-	type testType struct{ val int }
-	testData := []testType{{val: 1}, {val: 2}, {val: 3}}
-	assert.True(t, Any(testData, func(o testType) bool { return o.val == 2 }))
-	assert.False(t, Any(testData, func(o testType) bool { return o.val == 4 }))
-	assert.False(t, Any([]testType{}, func(o testType) bool { return o.val == 4 }))
-}
-
-func Test_All(t *testing.T) {
-	type testType struct{ val int }
-	testData := []testType{{val: 1}, {val: 2}, {val: 3}}
-	assert.True(t, All(testData, func(o testType) bool { return o.val > 0 }))
-	assert.False(t, All(testData, func(o testType) bool { return o.val > 1 }))
-	assert.True(t, All([]testType{}, func(o testType) bool { return o.val > 0 }))
-}
-
-func Test_FindAll(t *testing.T) {
-	type testType struct {
-		id   int
-		name string
-	}
-	testData := []testType{{id: 1, name: "foo"}, {id: 2, name: "bar"}, {id: 3, name: "baz"}, {id: 4, name: "foo2"}, {id: 5, name: "bar2"}}
-	expected := []testType{{id: 2, name: "bar"}, {id: 3, name: "baz"}, {id: 5, name: "bar2"}}
-	actual := FindAll(testData, func(v testType) bool { return strings.HasPrefix(v.name, "ba") })
-	assert.ElementsMatch(t, expected, actual)
-}
-
 func Test_Reduce(t *testing.T) {
 	t.Run("test 1", func(t *testing.T) {
 		type accumulation struct {
@@ -78,4 +51,42 @@ func Test_Reduce(t *testing.T) {
 		})
 		assert.Equal(t, map[string]int{"a": 2, "b": 1, "c": 3}, actual)
 	})
+}
+
+func Test_Any(t *testing.T) {
+	type testType struct{ val int }
+	testData := []testType{{val: 1}, {val: 2}, {val: 3}}
+	assert.True(t, Any(testData, func(o testType) bool { return o.val == 2 }))
+	assert.False(t, Any(testData, func(o testType) bool { return o.val == 4 }))
+	assert.False(t, Any([]testType{}, func(o testType) bool { return o.val == 4 }))
+}
+
+func Test_All(t *testing.T) {
+	type testType struct{ val int }
+	testData := []testType{{val: 1}, {val: 2}, {val: 3}}
+	assert.True(t, All(testData, func(o testType) bool { return o.val > 0 }))
+	assert.False(t, All(testData, func(o testType) bool { return o.val > 1 }))
+	assert.True(t, All([]testType{}, func(o testType) bool { return o.val > 0 }))
+}
+
+func Test_FindAll(t *testing.T) {
+	type testType struct {
+		id   int
+		name string
+	}
+	testData := []testType{{id: 1, name: "foo"}, {id: 2, name: "bar"}, {id: 3, name: "baz"}, {id: 4, name: "foo2"}, {id: 5, name: "bar2"}}
+	expected := []testType{{id: 2, name: "bar"}, {id: 3, name: "baz"}, {id: 5, name: "bar2"}}
+	actual := FindAll(testData, func(v testType) bool { return strings.HasPrefix(v.name, "ba") })
+	assert.ElementsMatch(t, expected, actual)
+}
+
+func Test_FindIndex(t *testing.T) {
+	type testType struct {
+		id   int
+		name string
+	}
+	testData := []testType{{id: 1, name: "foo"}, {id: 2, name: "bar"}, {id: 3, name: "baz"}, {id: 4, name: "foo2"}, {id: 5, name: "bar2"}}
+
+	assert.Equal(t, 2, FindIndex(testData, func(v testType) bool { return v.name == "baz" }))
+	assert.Equal(t, -1, FindIndex(testData, func(v testType) bool { return v.name == "oof" }))
 }
