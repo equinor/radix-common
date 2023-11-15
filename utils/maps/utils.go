4b4a4f120b584b3fbcc67b271047f2167b5aac9a
Sergey Smolnikov <ssmol@equinor.com>
2023-11-15 14:50:26 +0100

    Added maps convert utils (#28)

--- utils/maps/utils.go
+++ utils/maps/utils.go
@@ -1,5 +1,12 @@
 package maps
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/equinor/radix-common/utils/slice"
+)
+
 // GetKeysFromByteMap Returns keys. Deprecated - use GetKeysFromMap
 func GetKeysFromByteMap(mapData map[string][]byte) []string {
 	return GetKeysFromMap(mapData)
@@ -42,4 +49,31 @@ func MergeMaps[T comparable, V any](sources ...map[T]V) map[T]V {
 	}
 
 	return result
+}
+
+// FromString Converts a string with comma separated key-vaults to a map[string]string
+func FromString(keyValuePairs string) map[string]string {
+	return slice.Reduce(strings.Split(keyValuePairs, ","), make(map[string]string), func(acc map[string]string, pair string) map[string]string {
+		keyValue := strings.Split(pair, "=")
+		if len(keyValue) != 2 {
+			return acc
+		}
+		key := strings.TrimSpace(keyValue[0])
+		value := strings.TrimSpace(keyValue[1])
+		if len(key) > 0 && len(value) > 0 {
+			acc[key] = value
+		}
+		return acc
+	})
+}
+
+// ToString Converts map[string]string to a string with comma separated key-vaults, spaces are trimmed
+func ToString(keyValues map[string]string) string {
+	return strings.Join(slice.Reduce(GetKeysFromMap(keyValues), make([]string, 0), func(acc []string, key string) []string {
+		trimmedKey := strings.TrimSpace(key)
+		if len(trimmedKey) == 0 {
+			return acc
+		}
+		return append(acc, fmt.Sprintf("%s=%s", trimmedKey, strings.TrimSpace(keyValues[key])))
+	}), ",")
 }
