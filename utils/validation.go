b55ea0556879fd5092b904b2111371ba48b0898b
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-07-27 10:25:07 +0200

    fix(deps): update GitHub Actions and golang dependencies (#70)

--- utils/validation.go
+++ utils/validation.go
@@ -4,5 +4,5 @@ import "reflect"
 
 // IsNil check if the object is nil or an interface pointer contain nil
 func IsNil(obj interface{}) bool {
-	return obj == nil || (reflect.ValueOf(obj).Kind() == reflect.Ptr && reflect.ValueOf(obj).IsNil())
+	return obj == nil || (reflect.ValueOf(obj).Kind() == reflect.Pointer && reflect.ValueOf(obj).IsNil())
 }
